3fba276e4a4c991ea2624f9271e20ca9fc2f54c0
Jayesh Kumar Meena <iamjayesh23@gmail.com>
2019-11-14 10:30:32 +0530

    Add files via upload

---
+++ cloak.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+import time
+
+# cap = cv2.VideoCapture('/home/jayesh/Downloads/cloak.mp4')
+cap = cv2.VideoCapture(0)
+time.sleep(3)
+count=0
+background=0
+
+for i in range(60):
+    ret, background = cap.read()
+background = np.flip(background, axis = 1)
+
+while(1):
+    ret, img = cap.read()
+    if not ret:
+        break
+    count+=1
+    img = np.flip(img, axis = 1)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    
+    lower_green = np.array([46, 66, 51])
+    upper_green = np.array([75, 150, 130])
+    
+    lower_green = np.array([56,66,51])
+    upper_green = np.array([85,150,130])
+    mask2 = cv2.inRange(hsv,lower_green,upper_green)
+    
+    mask1 = cv2.inRange(hsv, lower_green, upper_green)
+    mask1 = cv2.morphologyEx(mask1, cv2.MORPH_OPEN, np.ones((3, 3), np.uint8), iterations=2)
+    mask1 = cv2.dilate(mask1, np.ones((3, 3), np.uint8), iterations = 1)
+    mask2 = cv2.bitwise_not(mask1)
+    
+    res1 = cv2.bitwise_and(background, background, mask = mask1)
+    res2 = cv2.bitwise_and(img, img, mask=mask2)
+    final_output = cv2.addWeighted(res1, 1, res2, 1, 0)
+    cv2.imshow("Invisible", final_output)
+    k = cv2.waitKey(10)
+    
+    if k==27:
+        break
